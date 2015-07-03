9a9343bb5330058e2fa1e9586467ef5258ea9c79
Arz Yu <arzyu@live.cn>
2015-07-03 15:38:27 +0800

    arz's dotfiles

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,48 +1,13 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
+# change tab size
+tabs -4
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
+export EDITOR='vim'
 
-fpath=($ZSH/functions $fpath)
+# use vi mode and bindkey
+bindkey -v
+bindkey '^r' history-incremental-search-backward
+bindkey '^p' up-line-or-search
+bindkey '^n' down-line-or-search
 
-autoload -U $ZSH/functions/*(:t)
-
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
-
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
-
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
-
-zle -N newtab
-
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+# kill Esc lag
+export KEYTIMEOUT=1
